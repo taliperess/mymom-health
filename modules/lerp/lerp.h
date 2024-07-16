6aa1d0a57d7da3ec5d3ad5da8a2557843cf01af3
Erik Gilling <konkers@google.com>
2024-07-16 21:28:39 +0000

    modules/lerp: Add lerp module
    Change-Id: I4112c8d1979a5572cdf0fadc9c613843e367c730
    Reviewed-on: https://pigweed-internal-review.git.corp.google.com/c/pigweed/showcase/rp2/+/71373
    Reviewed-by: Anthony DiGirolamo <tonymd@google.com>
    Commit-Queue: Erik Gilling <konkers@google.com>

---
+++ modules/lerp/lerp.h
@@ -0,0 +1,39 @@
+// Copyright 2024 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+#pragma once
+
+#include <cstdint>
+
+namespace am {
+/// Linearly interpolate between a and b using the specified fraction.
+///
+/// Does not do any bounds checking to ensure that the fraction is between
+/// 0 and 1.
+///
+/// @param  a          The A value for interpolation.
+/// @param  b          The B value for interpolation.
+/// @param numerator   The numerator of the fraction used to interpolate.
+/// @param denominator The denominator of the fraction used to interpolate.
+
+static constexpr uint8_t Lerp(uint8_t a,
+                              uint8_t b,
+                              uint16_t numerator,
+                              uint16_t denominator) {
+  // Do calculations in 32bit space to avoid overflows due to multiplication.
+  uint32_t a_32 = a;
+  uint32_t b_32 = b;
+
+  return static_cast<uint8_t>(a_32 + (b_32 - a_32) * numerator / denominator);
+}
+}  // namespace am
